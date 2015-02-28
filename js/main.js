60e9f42a5885a60892d665136e1a7faf354eea8a
Heather Laude <hlaude@ufl.edu>
2015-02-28 13:25:47 -0500

    add white listener

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 var blue = document.getElementById("blue");
+var white = document.getElementById("white");
 
 blue.addEventListener( "click", function() {
         var addPara = document.createElement("p");
         addPara.innerHTML = "Congrats! You see the dress how it actually is in real life.";
 
         blue.appendChild( addPara );
+});
+
+white.addEventListener( "click", function() {
+        var addPara = document.createElement("p");
+        addPara.innerHTML = "Rut row.  The real life dress isn't white and gold.  WHAAAAT?!";
+
+        white.appendChild( addPara );
 });
