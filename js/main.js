801a81d09760f5a4d1a951fccb8456de67d932f6
Heather Laude <hlaude@ufl.edu>
2015-02-28 13:55:17 -0500

    add comments to js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ var white = document.getElementById("white");
 var stupid = document.getElementById("stupid");
 var mindBlown = document.getElementById("mindblown");
 
+/*
+This Listener listens for someone to click the "blue and black" <p> and when they do, it appends a child to that <p> with the string below 
+*/
+
 blue.addEventListener( "click", function() {
         var addPara = document.createElement("p");
         addPara.innerHTML = "Congrats! You see the dress how it actually is in real life.";
@@ -10,12 +14,23 @@ blue.addEventListener( "click", function() {
         blue.appendChild( addPara );
 });
 
+
+
+/*
+This Listener listens for someone to click the "white and gold" <p> and when they do, it appends a child to that <p> with the string below 
+*/
+
 white.addEventListener( "click", function() {
         var addPara = document.createElement("p");
         addPara.innerHTML = "Rut row.  The real life dress isn't white and gold.  WHAAAAT?!";
 
         white.appendChild( addPara );
 });
+
+
+/*
+This Listener listens for someone to mouse over the "i think it's stupid" <p> and when they do, it looks up the div containing a meme with a display of none and changes the display to block. Then, on mouseout, it hides the div again.
+*/
 
 stupid.addEventListener( "mouseenter", function() {
         document.getElementById("stupid_hidden").style.display = "block";
@@ -26,6 +41,11 @@ stupid.addEventListener( "mouseout", function() {
         document.getElementById("stupid_hidden").style.display = "none";
 
 });
+
+
+/*
+This Listener listens for someone to mouse over the "my mind is blown" <p> and when they do, it looks up the div containing a meme with a display of none and changes the display to block. Then, on mouseout, it hides the div again.
+*/
 
 mindBlown.addEventListener( "mouseenter", function() {
         document.getElementById("mindblown_hidden").style.display = "block";
