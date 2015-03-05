9605063399d212c938c8594592c4e584d2641608
Richard Kalehoff <richardkalehoff@gmail.com>
2015-03-04 23:26:05 -0500

    prevent form from performing default submission/page refresh

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ var lastName = document.getElementById( "lastname" );
 var email = document.getElementById( "email_address" );
 var message = document.getElementById( "message" );
 
-formSubmit[0].addEventListener( "submit", function() {
+formSubmit[0].addEventListener( "submit", function(event) {
+        event.preventDefault();
         console.log("The firstName is: " + firstName.value);
         console.log("The lastName is: " + lastName.value);
         console.log("The email is: " + email.value);
